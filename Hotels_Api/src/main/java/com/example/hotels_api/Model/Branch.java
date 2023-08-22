da3063b56c2919e88640167a438b0f78323631bc
saleh <saleh@example.com>
2023-08-22 14:38:36 +0300

    add branch

--- Hotels_Api/src/main/java/com/example/hotels_api/Model/Branch.java
+++ Hotels_Api/src/main/java/com/example/hotels_api/Model/Branch.java
@@ -1,4 +1,39 @@
 package com.example.hotels_api.Model;
 
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Branch {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+    @NotEmpty(message = "can not be empty !")
+    @Column(columnDefinition = "varchar(20) not null")
+    private String name;
+
+    @NotEmpty(message = "can not be empty !")
+    @Column(columnDefinition = "varchar(20) not null")
+    private String neighborhood;
+
+    @NotEmpty(message = "can not be empty !")
+    @Column(columnDefinition = "varchar(20) not null")
+    private String neighborHood;
+
+
+    @NotEmpty(message = "can not be empty !")
+    @Size(min = 9,max = 9,message = "must be 9 digits")
+    @Column(columnDefinition = "varchar(9) not null")
+    private String phoneNumber;
 }
